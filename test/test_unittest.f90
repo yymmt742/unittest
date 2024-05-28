a5802ad3c88b90e2fe2ee31127cc6da59b3a3022
yymmt <yymmt@kuchem.kyoto-u.ac.jp>
2024-05-28 11:35:02 +0900

    [bugfix] CMakeLists.txt, nvfortran opts

--- test/test_unittest.f90
+++ test/test_unittest.f90
@@ -193,19 +193,17 @@ program main
     xe(i, j) = MERGE(1, 0, i==j)
   enddo
   call u%assert_is_eye(xe,                 'assert_is_eye        cmpx64')
-<<<<<<< HEAD
+  stop
   call random_number(rn)
   xe = rn - 0.5
   call random_number(rn)
   xe = xe + CMPLX(0.0, rn - 0.5)
   call u%assert_is_eye(xe,             'assert_is_eye        cmpx64')
   call u%assert_is_zero(xe,            'assert_is_zero       cmpx64')
-=======
-  stop
   call u%assert_is_zero(xe,                'assert_is_zero       cmpx64')
->>>>>>> 4be50c4... [update] tmp
+  call u%assert_is_zero(xe,                'assert_is_zero       cmpx64')
   xe = 0.0
-  call u%assert_is_zero(xe,            'assert_is_zero       cmpx64')
+  call u%assert_is_zero(xe,                'assert_is_zero       cmpx64')
 !&>
   call u%init('test_unittest', terminate_with_error_code=.false.)
   call u%assert(T,                         'assert               bool    0  ')
